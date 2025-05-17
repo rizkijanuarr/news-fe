e89bd391d2297d885c9fe5bc87348d939f4e3061
Rizki Januar I. <rizki.20046@mhs.unesa.ac.id>
2025-05-17 23:51:03 +0700

    🐛 fix : fix a something

--- src/views/Categories/Edit.jsx
+++ src/views/Categories/Edit.jsx
@@ -1,135 +1,168 @@
-//import react  
+//import react
 import { useState, useEffect } from "react";
 
 //import react router dom
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 //import layout
-import LayoutDefault from '../../layouts/Default';
+import LayoutDefault from "../../layouts/Default";
 
 //import api
-import Api from '../../api';
+import Api from "../../api";
 
 //import js cookie
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 //import toast
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 
 export default function CategoriesEdit() {
+  //title page
+  document.title = "Edit Categories - NewsApp Administartor";
 
-    //title page
-    document.title = "Edit Categories - NewsApp Administartor";
+  //navigate
+  const navigate = useNavigate();
 
-    //navigate
-    const navigate = useNavigate();
+  //get ID from parameter URL
+  const { id } = useParams();
 
-    //get ID from parameter URL
-    const { id } = useParams();
+  //define state for form
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [errors, setErrors] = useState({});
 
-    //define state for form
-    const [name, setName] = useState('');
-    const [image, setImage] = useState('');
-    const [errors, setErrors] = useState({});
+  //token from cookies
+  const token = Cookies.get("token");
 
-    //token from cookies
-    const token = Cookies.get('token');
+  //function "fetchDataCategories"
+  const fetchDataCategories = async () => {
+    await Api.get(`/api/v1/category/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      const data = response.data.data;
+      setName(data.name || "");
+      setImage(data.image || "");
+    });
+  };
 
-    //function "fetchDataCategories"
-    const fetchDataCategories = async () => {
-        await Api.get(`/api/v1/category/${id}`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            }
-        })
-        .then(response => {
-            const data = response.data.data;
-            setName(data.name || '');
-            setImage(data.image || '');
+  useEffect(() => {
+    fetchDataCategories();
+  }, []);
+
+  //function "updateCatgeories"
+  const updateCategories = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("image", image);
+    formData.append("name", name);
+
+    await Api.put(`/api/v1/category/${id}`, formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "content-type": "multipart/form-data",
+      },
+    })
+      .then((response) => {
+        toast.success("Categories updated successfully!", {
+          position: "top-right",
+          duration: 4000,
         });
-    }
+        navigate("/categories");
+      })
+      .catch((error) => {
+        setErrors(error.response?.data || {});
+      });
+  };
 
-    useEffect(() => {
-        fetchDataCategories();
-    }, []);
+  const resetForm = () => {
+    fetchDataCategories();
+    setErrors({});
+  };
 
-    //function "updateCatgeories"
-    const updateCategories = async (e) => {
-        e.preventDefault();
-        await Api.put(`/api/v1/category/${id}`, {
-            name,
-            image
-        }, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            }
-        })
-        .then(response => {
-            toast.success("Categories updated successfully!", {
-                position: "top-right",
-                duration: 4000,
-            });
-            navigate('/categories');
-        })
-        .catch(error => {
-            setErrors(error.response?.data || {});
-        })
-    }
-
-    const resetForm = () => {
-        fetchDataCategories();
-        setErrors({});
-    };
-
-    return (
-        <LayoutDefault>
-            <div className="container-fluid mb-5 mt-5">
-                <div className="row">
+  return (
+    <LayoutDefault>
+      <div className="container-fluid mb-5 mt-5">
+        <div className="row">
+          <div className="col-md-12">
+            <Link
+              to="/categories"
+              className="btn btn-md btn-tertiary border-0 shadow mb-3"
+              type="button"
+            >
+              <i className="fa fa-long-arrow-alt-left me-2"></i> Back
+            </Link>
+            <div className="card border-0 shadow">
+              <div className="card-body">
+                <h6>
+                  <i className="fa fa-user"></i> Edit Categories
+                </h6>
+                <hr />
+                <form onSubmit={updateCategories}>
+                  <div className="row">
+                    
                     <div className="col-md-12">
-                        <Link to="/categories" className="btn btn-md btn-tertiary border-0 shadow mb-3" type="button"><i className="fa fa-long-arrow-alt-left me-2"></i> Back</Link>
-                        <div className="card border-0 shadow">
-                            <div className="card-body">
-                                <h6><i className="fa fa-user"></i> Edit Categories</h6>
-                                <hr/>
-                                <form onSubmit={updateCategories}>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <div className="mb-3">
-                                                <label className="form-label fw-bold">Name</label>
-                                                <input type="text" className="form-control" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Name"/>
-                                            </div>
-                                            {errors.name && (
-                                                <div className="alert alert-danger">
-                                                    {errors.name[0]}
-                                                </div>
-                                            )}
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="mb-3">
-                                                <label className="form-label fw-bold">Image Link</label>
-                                                <input type="text" className="form-control" value={image} onChange={(e) => setImage(e.target.value)} placeholder="Enter Image Link"/>
-                                            </div>
-                                            {errors.image && (
-                                                <div className="alert alert-danger">
-                                                    {errors.image[0]}
-                                                </div>
-                                            )}
-                                        </div>
-                                    </div>
-
-                                
-                                    <hr/>
-                                    
-                                    <div>
-                                        <button type="submit" className="btn btn-md btn-tertiary me-2"><i className="fa fa-save"></i> Update</button>
-                                        <button type="button" onClick={resetForm} className="btn btn-md btn-warning"><i className="fa fa-redo"></i> Reset</button>
-                                    </div>
-                                </form>
-                            </div>
+                      <div className="mb-3">
+                        <label className="form-label fw-bold">Image</label>
+                        <input
+                          type="file"
+                          className="form-control"
+                          accept="image/*"
+                          onChange={(e) => setImage(e.target.files[0])}
+                        />
+                      </div>
+                      {errors.image && (
+                        <div className="alert alert-danger">
+                          {errors.image[0]}
                         </div>
+                      )}
                     </div>
-                </div>
+
+                    <div className="col-md-12">
+                      <div className="mb-3">
+                        <label className="form-label fw-bold">Name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                          placeholder="Enter Name"
+                        />
+                      </div>
+                      {errors.name && (
+                        <div className="alert alert-danger">
+                          {errors.name[0]}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+
+                  <hr />
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="btn btn-md btn-tertiary me-2"
+                    >
+                      <i className="fa fa-save"></i> Update
+                    </button>
+                    <button
+                      type="button"
+                      onClick={resetForm}
+                      className="btn btn-md btn-warning"
+                    >
+                      <i className="fa fa-redo"></i> Reset
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
-        </LayoutDefault>
-    )
+          </div>
+        </div>
+      </div>
+    </LayoutDefault>
+  );
 }
